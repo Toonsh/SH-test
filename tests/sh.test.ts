a3de3bb5dc1e0a64bb9b9ea215bc7c9c942b53b1
Toonsh <0807abc@toonation.co.kr>
2026-04-21 15:17:56 +0900

    remove: 테스트 파일 삭제

--- tests/sh.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('toon.at 접속', async ({ page }) => {
-  await page.goto('https://toon.at/');
-  await expect(page.locator('html')).toBeVisible();
-
-  // 브라우저 닫지 않고 대기
-  await page.pause();
-});
